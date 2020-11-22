edff8a22e65d8a53460b8ef012b7d356162ebd4f
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-11-21 18:18:15 -0600

    commit tree f37a2bd1430f420f308519247bb9254afc700b0f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_accessor :length_1, :length_2, :length_3, :sides
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    validate_triangle
+    if length_1 == length_2 && length_2 == length_3
+      :equilateral
+
+    elsif length_1 == length_2 || length_2 == length_3 || length_1 == length_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    real_triangle = [(length_1 + length_2 > length_3), (length_1 + length_3 > length_2), (length_2 + length_3 > length_1)]
+    [length_1, length_2, length_3].each do |side|
+      real_triangle << false if side <= 0
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
